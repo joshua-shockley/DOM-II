ef6a69be2d9f4e2dfa4a58418c167b81aeac7d63
Joshua-Shockley <thelumbashockley@gmail.com>
2019-08-06 12:44:38 -0500

    added greensock and some quick notes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+//create 10 unique event listeners to make this site more interactive.. some examples are as follows: mouseover, keydown, wheel, drag/drop, load, focus, resize, scroll, select and dblclick.. use to update DOM in creative ways. change colors, animate objects, remove objects, etc.
+
+// nest two similar events somwhere in the site and prevent event propagation properly
+//stop navigation from item from refreshing the page by using preventDefault()
